afc233ceb09a62519693e524d15e6aaeb7887980
Steve Honorio <steve@honors.com.br>
2020-05-02 21:50:28 -0300

    feat(customer): add migration

---
+++ db/migrate/20200501171150_create_customer.rb
@@ -0,0 +1,12 @@
+class CreateCustomer < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :document
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
