b79d862e92e405b0170f7d42473d765c839f6d56
Steve Honorio <steve@honors.com.br>
2020-05-03 04:29:39 -0300

    feat(account): add migration

---
+++ db/migrate/20200503035649_create_accounts.rb
@@ -0,0 +1,12 @@
+class CreateAccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :accounts, id: false do |t|
+      t.string :id, unique: true
+      t.string :name
+      t.decimal :balance, precision: 14, scale: 2
+      t.references :customer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_171150) do
+ActiveRecord::Schema.define(version: 2020_05_03_035649) do
+
+  create_table "accounts", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "id"
+    t.string "name"
+    t.decimal "balance", precision: 14, scale: 2
+    t.bigint "customer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_accounts_on_customer_id"
+  end
 
   create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_05_01_171150) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "accounts", "customers"
 end
