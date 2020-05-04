8f5cb89b8ded1cd39f2b12afd8eb34b268101ca1
Steve Honorio <steve@honors.com.br>
2020-05-04 15:34:27 -0300

    fix(account): migration

--- db/migrate/20200503035649_create_accounts.rb
+++ db/migrate/20200503035649_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts, id: false do |t|
-      t.string :id, unique: true
+      t.string :id, unique: true, primary_key: true
       t.string :name
       t.decimal :balance, precision: 14, scale: 2
       t.references :customer, foreign_key: true
