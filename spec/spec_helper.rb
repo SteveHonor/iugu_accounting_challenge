9f784b99ff399d01d7145791d7626c5b7745bd9f
Steve Honorio <steve@honors.com.br>
2020-05-04 15:32:13 -0300

    fix(config): test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,5 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.use_transactional_fixtures = false
-
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
