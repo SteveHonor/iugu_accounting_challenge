e5ad57d531be53ab1da178f35390173c5bbebf1a
Steve Honorio <steve@honors.com.br>
2020-05-04 15:38:45 -0300

    feat(account): router for action balance

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # Customers
-  post 'customers', controller: :customers,      action: :create, as: :customer
+  post 'customers', controller: :customers, action: :create, as: :customer
 
   # Authentication
-  post 'auth',      controller: :authentication, action: :auth
+  post 'auth', controller: :authentication, action: :auth
 
   # Account
-  post 'accounts',  controller: :accounts,       action: :create, as: :account
+  post 'accounts',            controller: :accounts, action: :create, as: :account
+  get  'balance/:account_id', controller: :accounts, action: :balance
+
   # Movement
   post 'transfer',  controller: :movements, action: :transfer, as: :movement
 end
