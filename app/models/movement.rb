11ae2521377fc420a3e25f9ad8530457a0de107d
Steve Honorio <steve@honors.com.br>
2020-05-04 15:28:42 -0300

    feat(movement): method for debit of account

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -6,4 +6,23 @@ class Movement < ApplicationRecord
     debit
   ].freeze
 
+  class << self
+    def create_debit account_id: nil, amount: 0
+      self.create!(
+        kind: :debit,
+        account_id: account_id,
+        amount: -amount
+      )
+
+      set_balance(account_id)
+    end
+
+
+    private
+
+    def set_balance(account_id)
+      account = Account.find(account_id)
+      account.update!(balance: account.movements.sum(:amount))
+    end
+  end
 end
