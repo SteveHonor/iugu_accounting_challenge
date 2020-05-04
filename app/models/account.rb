0dcb81afc2cabd6bc7e81d56a44b151b6c8098d7
Steve Honorio <steve@honors.com.br>
2020-05-04 15:38:20 -0300

    feat(account): return real monetary balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,10 +2,19 @@ class Account < ApplicationRecord
   before_validation :generate_id, on: :create
 
   belongs_to :customer
+  has_many :movements
 
   validates_uniqueness_of :id, case_sensitive: true
   validates :id, presence: true
   validates :name, presence: true
+
+  def check_not_limit amount
+    return true if (self.balance.to_number + amount.to_f) < 0
+  end
+
+  def balance
+    self[:balance].to_real
+  end
 
   private
 
